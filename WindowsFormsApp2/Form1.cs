8bd1927e05f4f555503ffe2f3c92aff0d18bf30d
pavlentytest <impetus@yandex.ru>
2019-02-12 11:36:51 +0000

    Первая версия
    Первая версия приложения по решению квадратного уравнения

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            double c = Convert.ToDouble(textBox3.Text);
+
+            double D = b * b - 4 * a * c;
+            if(D > 0)
+            {
+                double x1 = (-b - Math.Sqrt(D)) / (2 * a);
+                double x2 = (-b + Math.Sqrt(D)) / (2 * a);
+                label1.Text = "x1 = " + x1 + "; x2 = " + x2;
+            } else if(D==0)
+            {
+                double x = -b / (2 * a);
+                label1.Text = "x  = " + x;
+            }  else
+            {
+                label1.Text = "Нет решений";
+            }
+
+
+        }
+    }
+}
